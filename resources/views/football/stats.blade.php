b6e028c65ca875242514efc76e2e2911bb9e7315
Ali Javed <jali084337@gmail.com>
2025-09-26 16:18:12 +0500

    Fix: comment export button

--- resources/views/football/stats.blade.php
+++ resources/views/football/stats.blade.php
@@ -18,14 +18,14 @@
             <h1 class="text-3xl font-bold text-gray-800">Match Statistics</h1>
             <p class="text-gray-600 mt-2">Video: {{ $video->title ?? 'Match Video' }}</p>
         </div>
-        <div>
+        {{-- <div>
             <a href="{{ route('stats.download.pdf', $videoId) }}" class="px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700 transition-colors flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
                 </svg>
                 Download PDF
             </a>
-        </div>
+        </div> --}}
     </div>
 
     @if($teams && count($teams) >= 2)
